3d89c86576d647ea0524117aae8c1f5ac7b011da
Andy Baio <andy@waxy.org>
2012-04-16 23:25:00 -0700

    Now works with Chrome, updated data source

---
+++ README.md
@@ -0,0 +1,40 @@
+Memeorandum Colors
+==================
+
+Browser add-on to visualize linking bias on political websites.
+
+Installation
+------------
+
+Memeorandum Colors is a Greasemonkey user-script, which works 
+natively in Google Chrome or with the Greasemonkey plugin for
+Firefox.
+
+When installed, it will color links on Memeorandum based on
+each site's historical linking activity.  The brighter the
+color, the more they link only to articles in their same
+group.
+
+
+Using singular-
+
+
+### Google Chrome
+
+1. Click the memeorandum_colors.user.js link above.
+2. In the warning dialog at the bottom of Chrome window, select "Continue."
+2. Visit Memeorandum and wait a moment for the links to color.
+
+
+### Firefox
+
+1. Install [Greasemonkey](https://addons.mozilla.org/en-US/firefox/addon/greasemonkey/).
+2. Restart Firefox.
+3. Click the memeorandum_colors.user.js link above, wait three seconds, and Install.
+4. Visit Memeorandum and wait a moment for the links to color.
+
+
+Contributing
+------------
+
+Pull requests welcome!
---
+++ memeorandum_colors.user.js
@@ -0,0 +1,124 @@
+// Memeorandum Colors user script
+// version 0.3
+// 2012-02-16
+// Created by Andy Baio, http://waxy.org/
+// Data crunching by Joshua Schachter
+// Released under the GPL license
+// http://www.gnu.org/copyleft/gpl.html
+//
+// --------------------------------------------------------------------
+//
+// This is a Greasemonkey user script.
+//
+// To install, you need Greasemonkey: http://www.greasespot.net/
+// Then restart Firefox and revisit this script.
+// Under Tools, there will be a new menu item to "Install User Script".
+// Accept the default configuration and install.
+//
+// To uninstall, go to Tools/Manage User Scripts,
+// select "Memeorandum Colors", and click Uninstall.
+//
+// Thanks to Mark Pilgrim's Dive Into Greasemonkey for the guidance.
+//
+// --------------------------------------------------------------------
+//
+// ==UserScript==
+// @name          Memeorandum Colors
+// @namespace     http://waxy.org/2008/10/memeorandum_colors/
+// @description   Color Memeorandum stories based on linking bias
+// @include       http://memeorandum.com/*
+// @include       http://www.memeorandum.com/*
+// ==/UserScript==
+
+req = GM_xmlhttpRequest({
+    method: 'GET',
+    headers: {
+        'User-Agent': 'Mozilla/4.0 (compatible) Greasemonkey (gzip)',
+        'Accept': 'application/xml,text/xml',
+        'Accept-Encoding': 'gzip'
+    },
+    url: 'http://spreadsheets.google.com/feeds/list/0An6owk_2QmY7dDdSNVlGRXNIdjBsdTNSMUVqRHNyVGc/od6/public/values',
+    onload: function(responseDetails) {
+        if (responseDetails.status == 200) {
+            var parser = new DOMParser();
+            var xmlDoc = parser.parseFromString(responseDetails.responseText, "application/xml");
+            var entries = xmlDoc.getElementsByTagName('entry');
+
+            var scores = [];
+            for (var i = 0; i < entries.length; i++) {
+                var url = entries[i].getElementsByTagNameNS('http://schemas.google.com/spreadsheets/2006/extended','url')[0].textContent;
+                var score = entries[i].getElementsByTagNameNS('http://schemas.google.com/spreadsheets/2006/extended','score')[0].textContent;                
+                scores[url] = score;
+            }
+            names = colorExpanded(scores);                   
+            colorCollapsed(names);
+        }
+    }
+});
+
+function colorExpanded(scores) {
+    var allElements;
+    var names = [];
+    
+    allElements = document.evaluate(
+        "//div[@class='lnkr']/cite/a",
+        document,
+        null,
+        XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE,
+        null);
+        
+    for (var i = 0; i < allElements.snapshotLength; i++) {
+        a = allElements.snapshotItem(i);
+        memeUrl = a.href;
+        memeName = a.textContent;
+
+        if (scores[memeUrl]) {
+            var score = scores[memeUrl];
+            score = Math.round(score*1000)/10;            
+            // a.textContent += ' (' + score + ')';
+            
+            // generate a hex color based on the score
+            color = setColor(score);
+            a.style.backgroundColor = color;
+            if (score != 0) { a.style.textDecoration = 'none'; }
+            
+            // set color for rewriting collapsed view
+            names[memeName.substr(0,14)] = color;
+        }
+    }
+    return names;
+}
+
+function colorCollapsed(names) {
+    var allElements;
+    allElements = document.evaluate(
+        "//span[@class='mls']/a",
+        document,
+        null,
+        XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE,
+        null);
+        
+    for (var i = 0; i < allElements.snapshotLength; i++) {
+        a = allElements.snapshotItem(i);
+        memeName = a.textContent.substr(0,14);
+        if (names[memeName] != 'none') {
+            a.style.backgroundColor = names[memeName];
+            a.style.textDecoration = 'none';
+        }
+    }
+}
+
+function setColor(p) {
+    if (p < 0) {
+        if  (p < -10) { color = '#8686ff'; } 
+        else if (p < -5) { color = '#aaaaff'; } 
+        else { color = '#ccccff'; }
+    } else if (p > 0) {
+        if (p > 30) { color = '#ff6666'; } 
+        else if (p > 5) { color = '#ff9999'; } 
+        else { color = '#ffcccc'; }
+    } else {
+        color = 'none';
+    }
+    return color;
+}
